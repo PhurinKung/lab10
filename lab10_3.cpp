cd12280d1a9d1a1ae1d7f20e36b0981dbd6bfb75
pulin <purinza2006@gmail.com>
2025-12-23 15:15:04 +0700

    finish_lab10

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+
+int main(){
+    ifstream source("score.txt");
+    string score;
+    double sumformean = 0;
+    double sumforSD = 0;
+    int count = 0;
+    while(getline(source, score)){
+        count++;
+        sumformean += atof(score.c_str());
+        sumforSD += atof(score.c_str())*atof(score.c_str());
+    }
+    cout << "Number of data = "<< count << '\n';
+    cout << setprecision(3) ;
+    cout << "Mean = " << sumformean/count << '\n';
+    double m = sumformean/count;
+    cout << "Standard deviation = " << sqrt((sumforSD/count)-m*m);
+}
